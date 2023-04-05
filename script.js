9c6ca82c3211d9f6ed63bb64392fecb6213f067f
kellyetue@gmail.com <kellyetue@gmail.com>
2023-04-05 12:59:47 -0700

    les

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ h2.textContent = "Kelly Etue!";
 
 document.querySelector("body").appendChild(h2);
 
+addEventListener("click", (image))
+onclick = (image)
+const button = document.querySelector("button");
 
+button.addEventListener("click", (image))
